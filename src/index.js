7c9feb88b442857cd7d553d8222785391b5c8bc3
sumwai <sumwai@qq.com>
2023-01-26 21:41:18 +0800

    Fix hard-code

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square
+        key={i}
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
       />
@@ -21,23 +22,19 @@ class Board extends React.Component {
   }
 
   render() {
+    const rows = 3;
+    const arr = Array(rows).fill(null);
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {arr.map((_, x) => {
+          return (
+            <div key={x} className="board-row">
+              {arr.map((_, y) => {
+                return this.renderSquare(x * arr.length + y);
+              })}
+            </div>
+          );
+        })}
       </div>
     );
   }
